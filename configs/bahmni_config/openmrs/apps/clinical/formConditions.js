879349a9517de06f34e54ebe3a46fc011dd05501
Siddharth Vaish <sidvaish97@gmail.com>
2024-09-27 11:59:07 +0530

    Fix pom.xml and restructure

---
+++ configs/bahmni_config/openmrs/apps/clinical/formConditions.js
@@ -0,0 +1,33 @@
+Bahmni.ConceptSet.FormConditions.rules = {
+    'Blood Pressure' : function (formName, formFieldValues) {
+        var bloodPressure = formFieldValues['Blood Pressure'];
+        var regex = /\d/g;
+        if (regex.test(bloodPressure)) {
+            return {
+                enable: ["Posture"]
+            };
+        } else {
+            return {
+                disable: ["Posture"]
+            };
+        }
+    },
+    'Diastolic Data' : function (formName, formFieldValues) {
+        var diastolic = formFieldValues['Diastolic Data'];
+        if (diastolic) {
+            return {
+                enable: ["Posture"]
+            };
+        } else {
+            return {};
+        }
+    },
+    'Systolic Data' : function (formName, formFieldValues) {
+        var systolic = formFieldValues['Systolic Data'];
+        if (systolic) {
+            return {
+                enable: ["Posture"]
+            };
+        } else {}
+    }
+};
